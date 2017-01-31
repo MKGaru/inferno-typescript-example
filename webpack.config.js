3992808fd5634a98f958669b5abc6afc79f78be2
MKGaru <dev@igaru.com>
2017-01-31 16:21:33 +0900

    without babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,33 @@
+const path = require('path');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: "./src/index.tsx", // Point to main file
+	context: path.resolve(__dirname, './src'),
+	entry: "./index.tsx", // Point to main file
 	output: {
 		path: __dirname + "/dist",
 		filename: "bundle.js"
 	},
 	resolve: {
-		extensions: [ '.js', '.jsx', '.ts', '.tsx' ]
+		extensions: [ '.js', '.jsx', '.ts', '.tsx' ],
+		alias: {
+			'react': 'inferno-compat',
+			'react-dom': 'inferno-compat'
+		}
 	},
 	performance: {
-		hints: false
+		hints: false,
+		maxAssetSize: 5242880
 	},
 	module: {
 		loaders: [
 			{
 				test: /\.tsx?$/, 						  // All ts and tsx files will be process by
-				loaders: [ 'babel-loader', 'ts-loader' ], // first babel-loader, then ts-loader
+				loaders: [ 'ts-loader' ], 				  // only ts-loader without babel transpile
 				exclude: /node_modules/                   // ignore node_modules
-			}, {
-				test: /\.jsx?$/,                          // all js and jsx files will be processed by
-				loader: 'babel-loader',                   // babel-loader
-				exclude: /node_modules/                  // ignore node_modules
 			}
 		]
 	},
@@ -34,7 +38,7 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin(
 			{
-				template: "./src/index.html",
+				template: "./index.html",
 				inject: "body"
 			}
 		),
@@ -43,6 +47,14 @@ module.exports = {
 				verbose: true
 			}
 		),
+		new CopyWebpackPlugin([
+			{ from:'main.css' }
+		],{
+			copyUnmodified: false
+		}),
+		new webpack.ProvidePlugin({
+			'React.createElement': 'inferno/dist/inferno-create-element'
+		}),
 		// By default, webpack does `n=>n` compilation with entry files. This concatenates
 		// them into a single chunk.
 		new webpack.optimize.LimitChunkCountPlugin({
